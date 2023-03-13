17676527c2a020c84779f8a0e1ed9c31fa897e73
Helmut Neukirchen <helmut@hi.is>
2023-03-13 21:42:15 +0000

    Added main method (but the JUnit test are more thorough).

---
+++ src/main/java/is/hi/hbv202g/ass9/composite/Main.java
@@ -0,0 +1,19 @@
+package is.hi.hbv202g.ass9.composite;
+
+public class Main {
+
+	public static void main(String[] args) {
+		NumberLeaf number1 = new NumberLeaf(1);
+		NumberLeaf number2 = new NumberLeaf(2);
+		NumberLeaf number4 = new NumberLeaf(4);
+
+		PlusComposite plusComposite = new PlusComposite();
+		plusComposite.add(number1);
+		plusComposite.add(number2);
+
+		MultiplyComposite multiplyComposite = new MultiplyComposite();
+		multiplyComposite.add(number4);
+		multiplyComposite.add(plusComposite);
+
+		System.out.println("The result of ("+number1.getResult()+"+"+number2.getResult()+")*"+number4.getResult()+" is: "+ multiplyComposite.getResult());	}
+}
