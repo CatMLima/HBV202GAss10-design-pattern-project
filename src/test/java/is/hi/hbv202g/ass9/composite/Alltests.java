f38913cc1e2e2f337f9ec7ac9375c68abd20eecc
Helmut Neukirchen <helmut@hi.is>
2023-03-14 09:06:28 +0000

    Refactored test cases for composite package.

---
+++ src/test/java/is/hi/hbv202g/ass9/composite/Alltests.java
@@ -0,0 +1,10 @@
+package is.hi.hbv202g.ass9.composite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MixedCompositeTest.class, MultiplyCompositeTest.class, NumberLeafTest.class, PlusCompositeTest.class })
+public class Alltests {
+}
---
+++ src/test/java/is/hi/hbv202g/ass9/composite/MixedCompositeTest.java
@@ -0,0 +1,24 @@
+package is.hi.hbv202g.ass9.composite;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MixedCompositeTest {
+
+	@Test
+	public void testMixedPlusMultiplyComposites() {
+
+		NumberLeaf number1 = new NumberLeaf(1);
+		NumberLeaf number2 = new NumberLeaf(2);
+		NumberLeaf number4 = new NumberLeaf(4);
+
+		PlusComposite plusComposite = new PlusComposite();
+		plusComposite.add(number1);
+		plusComposite.add(number2);
+
+		MultiplyComposite multiplyComposite = new MultiplyComposite();
+		multiplyComposite.add(number4);
+		multiplyComposite.add(plusComposite);
+		assertEquals(12, multiplyComposite.getResult());
+	}
+}
